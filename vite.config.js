8df78ec5f3ce5b81b37ca3cdc171c27ccb50b49d
nafismumtaz <nafismumtaz@gmail.com>
2025-05-03 21:25:03 +0700

    "dist without proxy"

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '127.0.0.1',
-    port: 3000,
-    proxy:{
-      '/api': {
-        target: 'http://127.0.0.1:5000', // Ganti dengan URL backend Anda, misalnya ,  // URL backend Anda
-        changeOrigin: true,               // Menyesuaikan header Host
-        secure: false,                    // Nonaktifkan ini jika backend menggunakan self-signed HTTPS
-    } 
-  }
+    port: 3000
+    // proxy:{
+    //   '/api': {
+    //     target: 'http://127.0.0.1:5000', // Ganti dengan URL backend Anda, misalnya ,  // URL backend Anda
+    //     changeOrigin: true,               // Menyesuaikan header Host
+    //     secure: false,                    // Nonaktifkan ini jika backend menggunakan self-signed HTTPS
+    // } 
+  // }
 }});
